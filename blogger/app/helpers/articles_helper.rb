de75c4a572548c897b71c786747409be33ecbc87
Sparking <sparking02@yahoo.com.mx>
2020-09-01 20:06:47 -0500

    form-based workflow feature completed

--- blogger/app/helpers/articles_helper.rb
+++ blogger/app/helpers/articles_helper.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: false
+
+# Helper for the adding a new class.
 module ArticlesHelper
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
--- blogger/db/migrate/20200830231700_create_articles.rb
+++ blogger/db/migrate/20200830231700_create_articles.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: false
+
+# instructions for migrating the database
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
       t.string :title
       t.text :body
-      
+
       t.timestamps
     end
   end
